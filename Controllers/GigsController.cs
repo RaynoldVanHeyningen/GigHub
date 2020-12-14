f3931cd92d197ae8e16696c0671fe678536d959b
Raynold van Heyningen <raynoldvanheyningen@gmail.com>
2020-12-14 02:20:16 +0100

    Implement saving a gig to database

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -1,19 +1,27 @@
 using GigHub.Areas.Identity.Data;
+using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
+using System.Security.Claims;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
         private readonly GigHubIdentityDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public GigsController(GigHubIdentityDbContext context)
+        public GigsController(GigHubIdentityDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
+        [Authorize]
         public IActionResult Create()
         {
             var genres = _context.Genres.ToList();
@@ -24,6 +32,24 @@ namespace GigHub.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var gig = new Gig
+            {
+                ArtistId = _userManager.GetUserId(User),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
